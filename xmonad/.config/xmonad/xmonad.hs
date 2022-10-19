627a8040df20913d154d653b4e1636d1b2b7ba87
Dennis Vermeulen <dennis@wecreatepixels.nl>
2022-10-19 15:31:55 +0200

    Xmonad tweaks

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,65 +1,65 @@
-{-# LANGUAGE NamedFieldPuns #-}
-import XMonad
+import           XMonad
 
-import XMonad.Util.Run (spawnPipe)
-import XMonad.Layout
-import XMonad.Layout.NoBorders
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.Spacing
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
-import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
-import XMonad.Util.EZConfig (additionalKeysP)
-import XMonad.Util.SpawnOnce
-import XMonad.Hooks.SetWMName
+import           Data.Monoid
+import           XMonad.Hooks.EwmhDesktops      ( ewmh
+                                                , ewmhFullscreen
+                                                )
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers     ( doFullFloat
+                                                , isFullscreen
+                                                )
+import           XMonad.Layout.NoBorders
+import           XMonad.Layout.ResizableTile
+import           XMonad.Layout.Spacing
+import           XMonad.Util.EZConfig           ( additionalKeysP )
+import           XMonad.Util.SpawnOnce
 
-import Control.Monad (when, join)
-import Data.Monoid (All(..))
-import Data.Maybe (maybeToList)
-
+--brittany-disable-next-binding
 main :: IO ()
-main = do
-    xmonad $ docks $ ewmhFullscreen $ ewmh $ defaultConfig
-        { terminal = "alacritty"
-        , modMask = mod4Mask
-        , borderWidth = 2
-        , normalBorderColor = "#261E20"
-        , focusedBorderColor = "#FF1F66"
-        , layoutHook = myLayoutHook
-        , manageHook = myManageHook
-	, startupHook = myStartupHook
-        } `additionalKeysP` myKeys
+main = xmonad $ docks $ ewmhFullscreen $ ewmh $ def
+  { terminal = "alacritty"
+  , modMask = mod4Mask
+  , borderWidth = 2
+  , normalBorderColor = "#261E20"
+  , focusedBorderColor = "#FF1F66"
+  , layoutHook = myLayoutHook
+  , manageHook = myManageHook
+  , startupHook = myStartupHook
+  }
+  `additionalKeysP` myKeys
 
+myStartupHook :: X ()
 myStartupHook = do
-    spawnOnce "polybar top &"
-    spawnOnce "nitrogen --restore &"
+  spawnOnce "polybar top &"
+  spawnOnce "nitrogen --restore &"
 
-myLayoutHook = avoidStruts $ myLayouts
-    where
-        myLayouts  = tiled ||| Mirror tiled ||| full
+myLayoutHook = avoidStruts myLayouts
+ where
+  myLayouts = tiled ||| Mirror tiled ||| full
 
-        tiled      = spacing 5 $ smartBorders (ResizableTall nmaster delta ratio [])
-        full       = noBorders Full
+  tiled     = spacing 5 $ smartBorders (ResizableTall nmaster delta ratio [])
+  full      = noBorders Full
 
-        nmaster    = 1
-        delta      = 3/100
-        ratio = toRational (2/(1 + sqrt 5 :: Double))
+  nmaster   = 1
+  delta     = 3 / 100
+  ratio     = toRational (2 / (1 + sqrt 5 :: Double))
 
-myManageHook = composeAll
-    [ isFullscreen                  --> doFullFloat 
-    ]
+myManageHook :: Query (Data.Monoid.Endo WindowSet)
+myManageHook = composeAll [isFullscreen --> doFullFloat]
 
 cleanStart :: MonadIO m => m ()
-cleanStart = spawn $ "xmonad --recompile && xmonad --restart"
+cleanStart = spawn "xmonad --recompile && xmonad --restart"
 
 launchRofi :: MonadIO m => m ()
 launchRofi = spawn "rofi -show drun"
 
 launchShutdown :: MonadIO m => m ()
-launchShutdown = spawn "rofi -show power -modi power:$HOME/.config/rofi/power.sh"
+launchShutdown =
+  spawn "rofi -show power -modi power:$HOME/.config/rofi/power.sh"
 
-myKeys :: [(String, X())]
-myKeys = [ ( "M-p" , launchRofi ) -- dmenu app launcher
-         , ( "M-s" , launchShutdown ) -- dmenu app launcher
-         , ( "M-q" , cleanStart ) -- restart xmonad
-         ]
+myKeys :: [(String, X ())]
+myKeys =
+  [ ("M-p", launchRofi) -- dmenu app launcher
+  , ("M-s", launchShutdown) -- dmenu app launcher
+  , ("M-q", cleanStart) -- restart xmonad
+  ]
