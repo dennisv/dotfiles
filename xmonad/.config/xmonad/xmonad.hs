3e904ca4b6e0ba9791b3b55cac385d16b7353c3e
Dennis Vermeulen <dennis@wecreatepixels.nl>
2022-10-29 00:23:57 +0200

    Update xmonad spacing and tab theme

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,7 +1,18 @@
+import           Data.Bifunctor                 ( bimap )
+import           Data.Char                      ( isSpace )
+import           Data.List                      ( dropWhileEnd
+                                                , elemIndex
+                                                , find
+                                                )
+import           Data.Maybe                     ( catMaybes
+                                                , fromMaybe
+                                                )
+import           Data.Monoid                    ( Endo )
+import           System.IO.Unsafe               ( unsafeDupablePerformIO )
+
 import           XMonad
 import qualified XMonad.StackSet               as W
 
-import           Data.Monoid                    ( Endo )
 import           XMonad.Actions.SpawnOn         ( spawnOn )
 import           XMonad.Hooks.EwmhDesktops      ( addEwmhWorkspaceSort
                                                 , ewmh
@@ -25,6 +36,8 @@ import           XMonad.Layout.BoringWindows    ( BoringWindows
                                                 )
 import           XMonad.Layout.Decoration       ( Decoration
                                                 , DefaultShrinker
+                                                , Theme(..)
+                                                , shrinkText
                                                 )
 import           XMonad.Layout.LayoutModifier   ( ModifiedLayout )
 import           XMonad.Layout.NoBorders        ( SmartBorder
@@ -33,19 +46,22 @@ import           XMonad.Layout.NoBorders        ( SmartBorder
                                                 , smartBorders
                                                 )
 import           XMonad.Layout.ResizableTile    ( ResizableTall(ResizableTall) )
-import           XMonad.Layout.Simplest         ( Simplest )
+import           XMonad.Layout.Simplest         ( Simplest(Simplest) )
 import           XMonad.Layout.Spacing          ( Spacing
-                                                , spacing
+                                                , smartSpacingWithEdge
                                                 )
 import           XMonad.Layout.SubLayouts       ( GroupMsg(MergeAll, UnMerge)
                                                 , Sublayout
                                                 , onGroup
                                                 , pullGroup
-                                                , subTabbed
+                                                , subLayout
                                                 )
-import           XMonad.Layout.Tabbed           ( TabbedDecoration )
+import           XMonad.Layout.Tabbed           ( TabbedDecoration
+                                                , addTabs
+                                                )
 import           XMonad.Layout.WindowNavigation ( WindowNavigation
-                                                , windowNavigation
+                                                , configurableNavigation
+                                                , noNavigateBorders
                                                 )
 import           XMonad.Util.Cursor             ( setDefaultCursor )
 import           XMonad.Util.EZConfig           ( additionalKeysP )
@@ -55,6 +71,7 @@ import           XMonad.Util.NamedScratchpad    ( NamedScratchpad(NS)
                                                 , namedScratchpadManageHook
                                                 , scratchpadWorkspaceTag
                                                 )
+import           XMonad.Util.Run                ( runProcessWithInput )
 import           XMonad.Util.SpawnOnce          ( manageSpawn
                                                 , spawnOnce
                                                 )
@@ -68,19 +85,29 @@ myTerminal = "alacritty "
 myBrowser :: String
 myBrowser = "firefox"
 
---brittany-disable-next-binding
 main :: IO ()
-main = xmonad $ docks . addEwmhWorkspaceSort (pure myFilterEwmh) . ewmhFullscreen . ewmh $ def
-  { terminal = myTerminal
-  , modMask = mod4Mask
-  , borderWidth = 2
-  , normalBorderColor = "#261E20"
-  , focusedBorderColor = "#FF1F66"
-  , layoutHook = myLayoutHook
-  , manageHook = myManageHook <+> manageSpawn <+> namedScratchpadManageHook myScratchpads
-  , startupHook = myStartupHook
-  }
-  `additionalKeysP` myKeys
+main =
+  xmonad
+    . docks
+    . addEwmhWorkspaceSort (pure myFilterEwmh)
+    . ewmhFullscreen
+    . ewmh
+    $ myConfig
+ where
+  myConfig =
+    def
+        { terminal           = myTerminal
+        , modMask            = mod4Mask
+        , borderWidth        = 2
+        , normalBorderColor  = base00
+        , focusedBorderColor = base08
+        , layoutHook         = myLayoutHook
+        , manageHook         = myManageHook
+                               <+> manageSpawn
+                               <+> namedScratchpadManageHook myScratchpads
+        , startupHook        = myStartupHook
+        }
+      `additionalKeysP` myKeys
 
 myFilterEwmh :: WorkspaceSort
 myFilterEwmh = filterOutWs filterList
@@ -146,16 +173,32 @@ myLayoutHook = avoidStruts myLayouts
 
   tiled =
     smartBorders
-      $ boringWindows
-      $ windowNavigation
-      $ subTabbed
-      $ spacing 5
+      . boringWindows
+      . configurableNavigation noNavigateBorders
+      . addTabs shrinkText myTabTheme
+      . subLayout [] Simplest
+      . smartSpacingWithEdge 4
       $ ResizableTall nmaster delta ratio []
   full    = noBorders Full
 
   nmaster = 1
   delta   = 3 / 100
   ratio   = toRational (2 / (1 + sqrt 5 :: Double))
+
+myTabTheme :: Theme
+myTabTheme = def
+  { activeColor         = base08
+  , activeBorderColor   = base08
+  , activeTextColor     = basefg
+  , activeBorderWidth   = 0
+  , inactiveColor       = base00
+  , inactiveBorderColor = basebg
+  , inactiveTextColor   = base07
+  , inactiveBorderWidth = 2
+  , fontName = "xft:Iosevka:regular:size=9:antialias=true:hinting=true"
+  , decoHeight          = 25
+  , decoWidth           = maxBound
+  }
 
 myScratchpads :: [NamedScratchpad]
 myScratchpads =
@@ -204,3 +247,70 @@ myKeys =
   , ("M-p"  , launchRofi)
   , ("M-s"  , launchShutdown)
   ]
+
+{- |
+   Module : Theme.Theme
+   Copyright : (c) 2021 Joan Milev <joantmilev@gmail.com>
+   License : MIT
+
+   Maintainer : Joan Milev <joantmilev@gmail.com>
+   Stability : Stable
+   Portability : Unknown
+-}
+
+basebg, basefg, basecr, base00, base08, base01, base09, base02, base0A, base03, base0B, base04, base0C, base05, base0D, base06, base0E, base07, base0F
+  :: String
+basebg = xprop "*.background"
+basefg = xprop "*.foreground"
+basecr = xprop "*.cursorColor"
+base00 = xprop "*.color0"
+base08 = xprop "*.color8"
+base01 = xprop "*.color1"
+base09 = xprop "*.color9"
+base02 = xprop "*.color2"
+base0A = xprop "*.color10"
+base03 = xprop "*.color3"
+base0B = xprop "*.color11"
+base04 = xprop "*.color4"
+base0C = xprop "*.color12"
+base05 = xprop "*.color5"
+base0D = xprop "*.color13"
+base06 = xprop "*.color6"
+base0E = xprop "*.color14"
+base07 = xprop "*.color7"
+base0F = xprop "*.color15"
+
+-- myFont, myFontGTK, myBigFont, myBoldFont, myItalicFont :: String
+-- myFont = xprop "xmonad.font"
+-- myFontGTK = xprop "xmonad.font.gtk"
+-- myBigFont = xprop "xmonad.font.big"
+-- myBoldFont = xprop "xmonad.font.bold"
+-- myItalicFont = xprop "xmonad.font.italic"
+
+{- |
+   Module : Theme.Xresources
+   Copyright : (c) 2021 Joan Milev <joantmilev@gmail.com>
+   License : MIT
+
+   Maintainer : Joan Milev <joantmilev@gmail.com>
+   Stability : Stable
+   Portability : Unknown
+-}
+
+xProperty :: String -> IO String
+xProperty key =
+  fromMaybe "" . findValue key <$> runProcessWithInput "xrdb" ["-query"] ""
+
+findValue :: String -> String -> Maybe String
+findValue xresKey xres =
+  snd <$> find ((== xresKey) . fst) (catMaybes $ splitAtColon <$> lines xres)
+
+splitAtColon :: String -> Maybe (String, String)
+splitAtColon str = splitAtTrimming str <$> elemIndex ':' str
+
+splitAtTrimming :: String -> Int -> (String, String)
+splitAtTrimming str idx = bimap trim (trim . tail) $ splitAt idx str
+
+trim, xprop :: ShowS
+trim = dropWhileEnd isSpace . dropWhile isSpace
+xprop = unsafeDupablePerformIO . xProperty
