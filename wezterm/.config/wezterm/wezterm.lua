0124c9263f3030432d4b2b45ff38f3a2772f4d96
Dennis Vermeulen <dennis@wecreatepixels.nl>
2025-12-10 02:02:04 +0100

    Set wezterm background opacity

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = 'Arthur'
+config.window_background_opacity = 0.9
 config.font_size = 10.0
 
 return config
